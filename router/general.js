fe4ffd6e73a8b3bd7ac7b1436797959b44549426
Darrell Arcan <darrellarcan@gmail.com>
2026-06-30 00:04:59 +0700

    Add Axios Promise implementation

--- router/general.js
+++ router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -92,5 +93,81 @@ public_users.get('/review/:isbn', function (req, res) {
     return res.status(200).json(books[isbn].reviews);
 
 });
+// Task 11 - Get all books using Promise callback
+public_users.get("/promise/books", function (req, res) {
 
+    axios.get("http://localhost:5000/")
+        .then((response) => {
+            return res.status(200).json(response.data);
+        })
+        .catch((err) => {
+            return res.status(500).json({
+                message: err.message
+            });
+        });
+
+});
+
+// Task 11 - Get book by ISBN using Async/Await
+public_users.get("/promise/isbn/:isbn", async function (req, res) {
+
+    try {
+
+        const response = await axios.get(
+            `http://localhost:5000/isbn/${req.params.isbn}`
+        );
+
+        return res.status(200).json(response.data);
+
+    } catch (err) {
+
+        return res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
+
+// Task 11 - Get books by Author using Async/Await
+public_users.get("/promise/author/:author", async function (req, res) {
+
+    try {
+
+        const response = await axios.get(
+            `http://localhost:5000/author/${req.params.author}`
+        );
+
+        return res.status(200).json(response.data);
+
+    } catch (err) {
+
+        return res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
+
+// Task 11 - Get books by Title using Async/Await
+public_users.get("/promise/title/:title", async function (req, res) {
+
+    try {
+
+        const response = await axios.get(
+            `http://localhost:5000/title/${req.params.title}`
+        );
+
+        return res.status(200).json(response.data);
+
+    } catch (err) {
+
+        return res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
 module.exports.general = public_users;
